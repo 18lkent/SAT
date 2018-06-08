3d268213eeb3ddf6ed388303137a9cb9e59d76b2
18lkent <18lkent@mlmc.vic.edu.au>
2018-06-08 11:36:40 +1000

    Code completed on first day of developement. simple menu and inputs all created

---
+++ Core.py
@@ -0,0 +1,26 @@
+### Software created by Lachlan Kent ###
+   ### Begin date: Fri 8 jun 2018 ###
+
+
+
+print("1. Encode\n2. Decode\n3. Exit") #key for decisions
+running = 0
+
+while running == 0:
+    mode_input = str(input("Enter Decision: ")) #enter input for mode (encrpyt or decrypt)
+
+    if mode_input == "1": #If the user wishes to encrypt a file
+        file_input = input("Enter filename to Encrypt: ") #Asks the user to enter a text file to have its contents encrypted
+        f = open(file_input , "w") #opens the user specified file
+        running = 1
+
+    elif mode_input == "2":
+        file_input = input("Enter filename to Decrypt ") #Asks the user to enter a text file to have its contents decrypted
+        f = open(file_input, "w")  # opens the user specified file
+        running = 1
+
+    elif mode_input == "3":
+        exit(0)
+
+    else:
+        print("Invalid input detected, Try again!")
