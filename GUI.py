8a939e2d889ce0dfeb643e8c6b4541375853d0d9
18lkent <18lkent@mlmc.vic.edu.au>
2018-06-22 16:36:08 +1000

    GUI:
    working on Gui styling
    added button fonts
    
    TODO:
    - Style entry widgets
    - Settings page
    - Colour Changer
    - Implement fuctional code
    - Test both CLI and GUI code

--- GUI.py
+++ GUI.py
@@ -9,7 +9,7 @@ trimc = "#ff40e7"
 
 Besni = tk.Frame(root, bg=bgc)
 
-trim = tk.Frame(root, bg=trimc,width=800, height=3)
+trim = tk.Frame(root, bg=trimc,width=800, height=3,)
 trim.place(relx=0.0, rely=0.049)
 
 header = tk.Frame(root, bg=hdc,width=800,height=29)
@@ -17,14 +17,21 @@ header.grid(row=0,column=0,rowspan=29)
 
 generalb = tk.Button(root, text="General",highlightbackground='#272727')
 generalb.place(relx=0.0, rely=0.0, height=24, width=75)
+generalb.config(font=("Futura",15, "italic"))
 
 settingsb = tk.Button(root, text="Settings",highlightbackground='#272727')
 settingsb.place(relx=0.09, rely=0.0, height=24, width=75)
+settingsb.config(font=("Futura",15, "italic"))
 
-encodetext = tk.Text(root, height=25, width=47)
-encodetext.place(relx=0.05, rely=0.1)
+encodetextheader = tk.Frame(root, bg=hdc,width=341, height=31,)
+encodetextheader.place(relx=0.05, rely=0.1)
 
-decodetext = tk.Text(root, height=25, width=47)
+encodetext = tk.Text(root, height=25, width=47,bg = hdc, fg="white", borderwidth=2)
+encodetext.place(relx=0.05, rely=0.165)
+encodetext.config(highlightbackground=hdc)
+
+decodetext = tk.Text(root, height=25, width=47,bg = hdc, fg="white", borderwidth=2)
 decodetext.place(relx=0.53, rely=0.1)
+decodetext.config(highlightbackground=hdc)
 
 root.mainloop()
