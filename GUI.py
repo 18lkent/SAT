9a3ea48b1f2df206d89195cd65b06c152d12cbec
18lkent <18lkent@mlmc.vic.edu.au>
2018-06-22 22:36:36 +1000

    GUI:
    Commented code
    
    TODO:
    - Settings page
    - Colour/Theme Changer
    - Test both CLI and GUI code

--- GUI.py
+++ GUI.py
@@ -1,100 +1,114 @@
-import tkinter as tk
+import tkinter as tk #imports tkinter module as tk
 
 
 root = tk.Tk()
-settingsw = tk.Toplevel(root)
-settingsw.withdraw()
-root.title("Encoder/Decoder")
-root.geometry('800x600')
-root.resizable(False, False)
-root.configure(background="#515151")
-bgc = "#515151"
-hdc = "#272727"
-trimc = "#ff40e7"
+root.title("Encoder/Decoder") # gives title to gui
+root.geometry('800x600') # sets the window's size to 800px by 600px
+root.resizable(False, False) # disallows window resizing
+root.configure(background="#515151") # sets windows background colour as dark grey
+bgc = "#515151" # dark grey colour
+hdc = "#272727" # darker grey colour
+trimc = "#ff40e7" # pink colour
 
-Besni = tk.Frame(root, bg=bgc)
+########################################################################################################################
 
-### y = up and down, x = left and right
-trim = tk.Frame(root, bg=trimc,width=800, height=3,)
-trim.place(relx=0.0, rely=0.049)
+trim = tk.Frame(root, bg=trimc,width=800, height=3,) # defines the size and colour of the pink trim
+trim.place(relx=0.0, rely=0.049) # places the trim relative to the window (this is why i disabled window resizing)
 
-header = tk.Frame(root, bg=hdc,width=800,height=29)
-header.grid(row=0,column=0,rowspan=29)
+header = tk.Frame(root, bg=hdc,width=800,height=29) # sets toolbar size and colour
+header.grid(row=0,column=0,rowspan=29) # places toolbar at the top
 
-generalb = tk.Button(root, text="General",highlightbackground='#272727')
-generalb.place(relx=0.0, rely=0.0, height=24, width=75)
-generalb.config(font=("Futura",15, "italic"))
+########################################################################################################################
 
-settingsb = tk.Button(root, text="Settings",highlightbackground='#272727',command="Settings")
-settingsb.place(relx=0.09, rely=0.0, height=24, width=75)
-settingsb.config(font=("Futura",15, "italic"))
+generalb = tk.Button(root, text="General",highlightbackground='#272727') # creates general button
+generalb.place(relx=0.0, rely=0.0, height=24, width=75) # positions the button on the toolbar relative to the window
+generalb.config(font=("Futura",15, "italic")) # sets font and text size for general button
 
-encodetextheader = tk.Frame(root, bg=hdc,width=341, height=31,)
-encodetextheader.place(relx=0.05, rely=0.1)
+settingsb = tk.Button(root, text="Settings",highlightbackground='#272727') # creates settings button
+settingsb.place(relx=0.09, rely=0.0, height=24, width=75) # positions the button on the toolbar relative to the window
+settingsb.config(font=("Futura",15, "italic")) # sets font and text size for general button
 
-encodetextheaderlabel = tk.Label(root, text="Encode", font=("Futura",15, "italic"))
-encodetextheaderlabel.place(relx=0.212, rely=0.102, height=24, width=75)
-encodetextheaderlabel.config(bg=hdc,fg="white")
+########################################################################################################################
 
-encodetext = tk.Text(root, height=25, width=47,bg = hdc, fg="white", borderwidth=2)
-encodetext.place(relx=0.05, rely=0.165)
-encodetext.config(highlightbackground=hdc)
+encodetextheader = tk.Frame(root, bg=hdc,width=341, height=31,) # makes box for the header above the encoder text box
+encodetextheader.place(relx=0.05, rely=0.1) # places the header relative to the window
 
-decodetextheader = tk.Frame(root, bg=hdc,width=341, height=31,)
-decodetextheader.place(relx=0.53, rely=0.1)
+encodetextheaderlabel = tk.Label(root, text="Encode", font=("Futura",15, "italic")) # creates the label above the encoder
+encodetextheaderlabel.place(relx=0.212, rely=0.102, height=24, width=75) # positions the label in the header
+encodetextheaderlabel.config(bg=hdc,fg="white") # makes the text white and the background of the label the same colour as the header
 
-decodetextheaderlabel = tk.Label(root, text="Decode", font=("Futura",15, "italic"))
-decodetextheaderlabel.place(relx=0.687, rely=0.102, height=24, width=75)
-decodetextheaderlabel.config(bg=hdc,fg="white")
+encodetext = tk.Text(root, height=25, width=47,bg = hdc, fg="white", borderwidth=2) # creates the text box
+encodetext.place(relx=0.05, rely=0.165) # places the text box on the screen relative to the window
+encodetext.config(highlightbackground=hdc) # makes the highlight border the same colour as the text box
 
-decodetext = tk.Text(root, height=25, width=47,bg = hdc, fg="white", borderwidth=2)
-decodetext.place(relx=0.53, rely=0.165)
-decodetext.config(highlightbackground=hdc)
+########################################################################################################################
 
-def convert():
-    inputd = decodetext.get("1.0",'end-1c')
-    inpute = encodetext.get("1.0", 'end-1c')
+decodetextheader = tk.Frame(root, bg=hdc,width=341, height=31,) # makes the box fo the header aboce the decoder text box
+decodetextheader.place(relx=0.53, rely=0.1) # places the header relative to the window
+
+decodetextheaderlabel = tk.Label(root, text="Decode", font=("Futura",15, "italic")) # creates the label above the decoder
+decodetextheaderlabel.place(relx=0.687, rely=0.102, height=24, width=75) # positions the label in the header
+decodetextheaderlabel.config(bg=hdc,fg="white") # makes the text white and the background of the label the same colour as the header
+
+decodetext = tk.Text(root, height=25, width=47,bg = hdc, fg="white", borderwidth=2) # creates the text box
+decodetext.place(relx=0.53, rely=0.165) # positions the text box on the screen relative to the window
+decodetext.config(highlightbackground=hdc) # makes the highlight border the same colour as the text box
+
+########################################################################################################################
+
+def convert(): # defines function called "convert"
+    inputd = decodetext.get("1.0",'end-1c') # gets the input from the decode text box
+    inpute = encodetext.get("1.0", 'end-1c') # gets the input from the encode text box
     if inputd != "":
-        encodetext.delete("1.0", 'end-1c')
+        encodetext.delete("1.0", 'end-1c') #deletes everything in the encodetext box
         inputdc = inputd.replace("F4", "a").replace("A4", "b").replace("A1", "c").replace("E3", "d").replace("B6","e").replace("D1", "f").replace("C4", "g").replace("B5", "h").replace("C2", "i").replace("D3", "j").replace("F3", "k").replace("E6", "l").replace("A2", "m").replace("D5", 'n').replace("B3", "o").replace("A6", "p").replace("D6","q").replace("B1", "r").replace("D2", "s").replace("B4", "t").replace("A5", "u").replace("B2", "v").replace("A3","w").replace("F5", "x").replace("C6", "y").replace("C1", "z").replace("4!"," ").replace("f4", "A").replace("a4", "B").replace("a1", "C").replace("e3", "D").replace("b6","E").replace("d1", "F").replace("c4", "G").replace("b5", "H").replace("c2", "I").replace("d3", "J").replace("f3", "K").replace("e6", "L").replace("a2", "M").replace("d5", 'N').replace("b3", "O").replace("a6", "P").replace("d6","Q").replace("b1", "R").replace("d2", "S").replace("b4", "T").replace("a5", "U").replace("b2", "V").replace("a3","W").replace("f5", "X").replace("c6", "Y").replace("c1", "Z")
-        encodetext.insert('end', inputdc)
+        encodetext.insert('end', inputdc) #inserts encoded text in the decode box # replace algorithm ^
     elif inpute != "":
-        decodetext.delete("1.0", 'end-1c')
+        decodetext.delete("1.0", 'end-1c') #deletes everything in the decodetext box
         inputec = inpute.replace("A", "f4").replace("B", "a4").replace("C", "a1").replace("D", "e3").replace("E", "b6").replace("F","d1").replace("G", "c4").replace("H", "b5").replace("I", "c2").replace("J", "d3").replace("K", "f3").replace("L","e6").replace("M", "a2").replace("N", 'd5').replace("O", "b3").replace("P", "a6").replace("Q", "d6").replace("R","b1").replace("S", "d2").replace("T", "b4").replace("U", "a5").replace("V", "b2").replace("W", "a3").replace("X","f5").replace("Y", "c6").replace("Z", "c1").replace(" ", "4!").replace("a", "F4").replace("b", "A4").replace("c","A1").replace("d", "E3").replace("e", "B6").replace("f", "D1").replace("g", "C4").replace("h", "B5").replace("i","C2").replace("j", "D3").replace("k", "F3").replace("l", "E6").replace("m", "A2").replace("n", 'D5').replace("o","B3").replace("p", "A6").replace("q", "D6").replace("r", "B1").replace("s", "D2").replace("t", "B4").replace("u","A5").replace("v", "B2").replace("w", "A3").replace("x", "F5").replace("y", "C6").replace("z", "C1")
-        decodetext.insert('end', inputec)
+        decodetext.insert('end', inputec) #inserts decoded text in the encode box # replace algorithm ^
 
-convertb = tk.Button(root, text="Convert",highlightbackground=bgc, command = convert)
-convertb.place(relx=0.4575, rely=0.83, height=24, width=75)
-convertb.config(font=("Futura",15, "italic"))
+########################################################################################################################
 
-def copyd():
-    copyd2 = decodetext.get("1.0", 'end-1c')
-    root.clipboard_clear()
-    root.clipboard_append(str(copyd2))
-    root.update()
+convertb = tk.Button(root, text="Convert",highlightbackground=bgc, command = convert) # creates button and assigns convert command
+convertb.place(relx=0.4575, rely=0.83, height=24, width=75) # places convert button relative to window
+convertb.config(font=("Futura",15, "italic")) # configures button font
 
-def copye():
-    copye2 = encodetext.get("1.0", 'end-1c')
-    root.clipboard_clear()
-    root.clipboard_append(str(copye2))
-    root.update()
+########################################################################################################################
 
+def copyd(): # defines copy button for the decoder
+    copyd2 = decodetext.get("1.0", 'end-1c') # gets text from the decoder box
+    root.clipboard_clear() # clears users clipboard
+    root.clipboard_append(str(copyd2)) # adds string from text box to users clipboard
+    root.update() # this makes it so that the clipboard remains when the window is closed
 
-copydb = tk.Button(root, text="Copy",highlightbackground=bgc, command=copyd)
-copydb.place(relx=0.867, rely=0.83, height=24, width=75)
-copydb.config(font=("Futura",15, "italic"))
+copydb = tk.Button(root, text="Copy",highlightbackground=bgc, command=copyd) # creates the copy button and assigns the copyd command
+copydb.place(relx=0.867, rely=0.83, height=24, width=75) # establishes proportions and location of the button relative to the window
+copydb.config(font=("Futura",15, "italic")) # configures text on the button
 
-copyeb = tk.Button(root, text="Copy",highlightbackground=bgc, command=copye)
-copyeb.place(relx=0.0465, rely=0.83, height=24, width=75)
-copyeb.config(font=("Futura",15, "italic"))
+########################################################################################################################
 
-def close():
-    exit(0)
+def copye(): # defines copy button for the encoder
+    copye2 = encodetext.get("1.0", 'end-1c') # gets text from the encoder box
+    root.clipboard_clear() # clears use clipboard
+    root.clipboard_append(str(copye2)) # adds string from text box to users clipboard
+    root.update() # this makes it so that the clipboard remains when the window is closed
 
-closeb = tk.Button(root, text="Close",highlightbackground=bgc, command=close)
-closeb.place(relx=0.9, rely=0.95, height=24, width=75)
-closeb.config(font=("Futura",15, "italic"))
+copyeb = tk.Button(root, text="Copy",highlightbackground=bgc, command=copye) # creates the copy button and assigns the copye command
+copyeb.place(relx=0.0465, rely=0.83, height=24, width=75) # establishes proportions and location of the button relative to the window
+copyeb.config(font=("Futura",15, "italic")) # configures text on the button
 
-root.mainloop()
+########################################################################################################################
 
+def close(): # defines close function
+    exit(0) # exits code with code 0
 
+########################################################################################################################
+
+closeb = tk.Button(root, text="Close",highlightbackground=bgc, command=close) # creates close button and assigns close command
+closeb.place(relx=0.9, rely=0.95, height=24, width=75) # places button and defines size
+closeb.config(font=("Futura",15, "italic")) # configures text on the button
+
+########################################################################################################################
+
+root.mainloop() # starts mainloop
