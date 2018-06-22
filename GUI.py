895319ac0013ce60906de0e8679bef4be60a3789
18lkent <18lkent@mlmc.vic.edu.au>
2018-06-22 15:47:00 +1000

    GUI:
    Finalized toolbar style.
    
    TODO:
    - Style entry widgets
    - Settings page
    - Colour Changer
    - Implement fuctional code
    - Test both CLI and GUI code

--- GUI.py
+++ GUI.py
@@ -7,21 +7,24 @@ bgc = "#515151"
 hdc = "#272727"
 trimc = "#ff40e7"
 
-
-size = tk.Frame(root)
 Besni = tk.Frame(root, bg=bgc)
-header = tk.Frame(root, bg=hdc,width=800,height=29)
-
-header.grid(row=0,column=0)#,columnspan=29)
-
-general = tk.Button(root, text="General",highlightbackground='#272727')
-general.grid(row=0,column=0)
 
 trim = tk.Frame(root, bg=trimc,width=800, height=3)
-trim.grid(row=0,column=0)
+trim.place(relx=0.0, rely=0.049)
 
-#settings = tk.Button(root, text="settings",highlightbackground='#272727')
-#settings.grid(row=0,column=1)
+header = tk.Frame(root, bg=hdc,width=800,height=29)
+header.grid(row=0,column=0,rowspan=29)
 
+generalb = tk.Button(root, text="General",highlightbackground='#272727')
+generalb.place(relx=0.0, rely=0.0, height=24, width=75)
+
+settingsb = tk.Button(root, text="Settings",highlightbackground='#272727')
+settingsb.place(relx=0.09, rely=0.0, height=24, width=75)
+
+encodetext = tk.Text(root, height=25, width=47)
+encodetext.place(relx=0.05, rely=0.1)
+
+decodetext = tk.Text(root, height=25, width=47)
+decodetext.place(relx=0.53, rely=0.1)
 
 root.mainloop()
