5753ceef9c4f340c1a896b33543543d4b38bcc1a
18lkent <18lkent@mlmc.vic.edu.au>
2018-06-25 19:25:43 +1000

    GUI:
    Fixed GUI and removed Convert Button to make the converter easier
    
    TODO:
    - Comment Code
    - Fix known errors with CLI and GUI
    - add all features to settings page
    - Test both CLI and GUI code

--- GUI.py
+++ GUI.py
@@ -60,10 +60,8 @@ def settingsb():
     decodetext.lower()
     decodetextheaderlabel.lower()
     decodetextshadow.lower()
-    convertb.lower()
     copydb.lower()
     copyeb.lower()
-    convertb.lower()
     popupMenu.tkraise()
     popupMenuheader.tkraise()
     popupMenutitle.tkraise()
@@ -83,10 +81,8 @@ def generalb():
     decodetextheader.tkraise()
     decodetext.tkraise()
     decodetextheaderlabel.tkraise()
-    convertb.tkraise()
     copydb.tkraise()
     copyeb.tkraise()
-    convertb.tkraise()
     popupMenu.lower()
     popupMenuheader.lower()
     popupMenutitle.lower()
@@ -176,27 +172,20 @@ decodetext.config(highlightbackground=hdc) # makes the highlight border the same
 ########################################################################################################################
 # Conversion Function                                                                                                  #
 ########################################################################################################################
-
-def convert(): # defines function called "convert"
-    inputd = decodetext.get("1.0",'end-1c') # gets the input from the decode text box
+def encodethetext( event ):
     inpute = encodetext.get("1.0", 'end-1c') # gets the input from the encode text box
-    if inputd != "":
-        encodetext.delete("1.0", 'end-1c') #deletes everything in the encodetext box
-        inputdc = inputd.replace("F4", "a").replace("A4", "b").replace("A1", "c").replace("E3", "d").replace("B6","e").replace("D1", "f").replace("C4", "g").replace("B5", "h").replace("C2", "i").replace("D3", "j").replace("F3", "k").replace("E6", "l").replace("A2", "m").replace("D5", 'n').replace("B3", "o").replace("A6", "p").replace("D6","q").replace("B1", "r").replace("D2", "s").replace("B4", "t").replace("A5", "u").replace("B2", "v").replace("A3","w").replace("F5", "x").replace("C6", "y").replace("C1", "z").replace("4!"," ").replace("f4", "A").replace("a4", "B").replace("a1", "C").replace("e3", "D").replace("b6","E").replace("d1", "F").replace("c4", "G").replace("b5", "H").replace("c2", "I").replace("d3", "J").replace("f3", "K").replace("e6", "L").replace("a2", "M").replace("d5", 'N').replace("b3", "O").replace("a6", "P").replace("d6","Q").replace("b1", "R").replace("d2", "S").replace("b4", "T").replace("a5", "U").replace("b2", "V").replace("a3","W").replace("f5", "X").replace("c6", "Y").replace("c1", "Z")
-        encodetext.insert('end', inputdc) #inserts encoded text in the decode box # replace algorithm ^
-    elif inpute != "":
-        decodetext.delete("1.0", 'end-1c') #deletes everything in the decodetext box
-        inputec = inpute.replace("A", "f4").replace("B", "a4").replace("C", "a1").replace("D", "e3").replace("E", "b6").replace("F","d1").replace("G", "c4").replace("H", "b5").replace("I", "c2").replace("J", "d3").replace("K", "f3").replace("L","e6").replace("M", "a2").replace("N", 'd5').replace("O", "b3").replace("P", "a6").replace("Q", "d6").replace("R","b1").replace("S", "d2").replace("T", "b4").replace("U", "a5").replace("V", "b2").replace("W", "a3").replace("X","f5").replace("Y", "c6").replace("Z", "c1").replace(" ", "4!").replace("a", "F4").replace("b", "A4").replace("c","A1").replace("d", "E3").replace("e", "B6").replace("f", "D1").replace("g", "C4").replace("h", "B5").replace("i","C2").replace("j", "D3").replace("k", "F3").replace("l", "E6").replace("m", "A2").replace("n", 'D5').replace("o","B3").replace("p", "A6").replace("q", "D6").replace("r", "B1").replace("s", "D2").replace("t", "B4").replace("u","A5").replace("v", "B2").replace("w", "A3").replace("x", "F5").replace("y", "C6").replace("z", "C1")
-        decodetext.insert('end', inputec) #inserts decoded text in the encode box # replace algorithm ^
+    decodetext.delete("1.0", 'end-1c') #deletes everything in the decodetext box
+    inputec = inpute.replace("A", "f4").replace("B", "a4").replace("C", "a1").replace("D", "e3").replace("E", "b6").replace("F","d1").replace("G", "c4").replace("H", "b5").replace("I", "c2").replace("J", "d3").replace("K", "f3").replace("L","e6").replace("M", "a2").replace("N", 'd5').replace("O", "b3").replace("P", "a6").replace("Q", "d6").replace("R","b1").replace("S", "d2").replace("T", "b4").replace("U", "a5").replace("V", "b2").replace("W", "a3").replace("X","f5").replace("Y", "c6").replace("Z", "c1").replace(" ", "4!").replace("a", "F4").replace("b", "A4").replace("c","A1").replace("d", "E3").replace("e", "B6").replace("f", "D1").replace("g", "C4").replace("h", "B5").replace("i","C2").replace("j", "D3").replace("k", "F3").replace("l", "E6").replace("m", "A2").replace("n", 'D5').replace("o","B3").replace("p", "A6").replace("q", "D6").replace("r", "B1").replace("s", "D2").replace("t", "B4").replace("u","A5").replace("v", "B2").replace("w", "A3").replace("x", "F5").replace("y", "C6").replace("z", "C1")
+    decodetext.insert('end', inputec) #inserts decoded text in the encode box # replace algorithm ^
 
-########################################################################################################################
-# Convert Button                                                                                                       #
-########################################################################################################################
+def decodethetext( event ):
+    inputd = decodetext.get("1.0",'end-1c') # gets the input from the decode text box
+    encodetext.delete("1.0", 'end-1c') #deletes everything in the encodetext box
+    inputdc = inputd.replace("F4", "a").replace("A4", "b").replace("A1", "c").replace("E3", "d").replace("B6","e").replace("D1", "f").replace("C4", "g").replace("B5", "h").replace("C2", "i").replace("D3", "j").replace("F3", "k").replace("E6", "l").replace("A2", "m").replace("D5", 'n').replace("B3", "o").replace("A6", "p").replace("D6","q").replace("B1", "r").replace("D2", "s").replace("B4", "t").replace("A5", "u").replace("B2", "v").replace("A3","w").replace("F5", "x").replace("C6", "y").replace("C1", "z").replace("4!"," ").replace("f4", "A").replace("a4", "B").replace("a1", "C").replace("e3", "D").replace("b6","E").replace("d1", "F").replace("c4", "G").replace("b5", "H").replace("c2", "I").replace("d3", "J").replace("f3", "K").replace("e6", "L").replace("a2", "M").replace("d5", 'N').replace("b3", "O").replace("a6", "P").replace("d6","Q").replace("b1", "R").replace("d2", "S").replace("b4", "T").replace("a5", "U").replace("b2", "V").replace("a3","W").replace("f5", "X").replace("c6", "Y").replace("c1", "Z")
+    encodetext.insert('end', inputdc) #inserts encoded text in the decode box # replace algorithm ^
 
-convertb = tk.Button(root, text="Convert",highlightbackground=bgc, command = convert) # creates button and assigns convert command
-convertb.place(relx=0.4575, rely=0.83, height=24, width=75) # places convert button relative to window
-convertb.config(font=("Futura",15, "italic")) # configures button font
-
+decodetext.bind("<KeyRelease>", decodethetext)
+encodetext.bind("<KeyRelease>", encodethetext)
 ########################################################################################################################
 # Copy decode function and button                                                                                        #
 ########################################################################################################################
